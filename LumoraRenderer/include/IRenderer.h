0b108be0d44f0005e2c4d187684ddc8112f87636
GD <nkari82@gmail.com>
2025-01-02 03:11:44 +0900

    Cleanup

--- LumoraRenderer/include/IRenderer.h
+++ LumoraRenderer/include/IRenderer.h
@@ -5,102 +5,103 @@
 #include <memory>
 #include <string>
 
-namespace Lumora
-{
+// 이 헤더는 vulkan, vma 등 어떤 의존성도 드러내지 않음
 
-    // 핸들 정의
-    using SwapChainHandle = uint64_t;
-    using BufferHandle = uint64_t;
-    using TextureHandle = uint64_t;
-    using SamplerHandle = uint64_t;
-    using PipelineHandle = uint64_t;
-    using ShaderHandle = uint64_t;
+namespace Lumora {
 
-    // 스왑체인 파라미터
-    struct SwapChainDesc
-    {
-        // Win32라면 HWND가 될 수 있습니다.
-        void *window_handle = nullptr;
-        int width = 1280;
-        int height = 720;
-        bool vsync = true;
-    };
+// 리소스 핸들
+using SwapChainHandle = uint64_t;
+using BufferHandle = uint64_t;
+using TextureHandle = uint64_t;
+using SamplerHandle = uint64_t;
+using PipelineHandle = uint64_t;
+using ShaderHandle = uint64_t;
 
-    // 버퍼, 텍스처, 샘플러, 파이프라인 등 각종 Desc 구조체들
-    struct BufferDesc
-    {
-        size_t size_in_bytes = 0;
-        bool usage_uniform_buffer = false;
-        bool usage_vertex_buffer = false;
-        bool usage_index_buffer = false;
-        bool usage_transfer_src = false;
-        bool usage_transfer_dst = false;
-    };
+// 스왑체인
+struct SwapChainDesc {
+    void* window_handle = nullptr;  // Win32: HWND
+    int width = 1280;
+    int height = 720;
+    bool vsync = true;
+};
 
-    struct TextureDesc
-    {
-        uint32_t width = 0;
-        uint32_t height = 0;
-    };
+// 버퍼
+struct BufferDesc {
+    size_t size_in_bytes = 0;
+    bool usage_uniform_buffer = false;
+    bool usage_vertex_buffer = false;
+    bool usage_index_buffer = false;
+    bool usage_transfer_src = false;
+    bool usage_transfer_dst = false;
+};
 
-    struct SamplerDesc
-    {
-        // 필터/주소모드 등
-    };
+// 텍스처
+struct TextureDesc {
+    uint32_t width = 0;
+    uint32_t height = 0;
+    bool is_cube_map = false;  // 앞으로의 과제 (큐브맵)
+    bool is_3d = false;        // 3D 텍스처
+                               // mipLevels, arrayLayers, format, etc. 확장 가능
+};
 
-    struct ShaderDesc
-    {
-        std::string file_path; // SPIR-V 등
-    };
+// 샘플러
+struct SamplerDesc {
+    // 필터링, 어드레스 모드, mipmap 모드 등
+};
 
-    struct PipelineDesc
-    {
-        ShaderHandle vertex_shader = 0;
-        ShaderHandle fragment_shader = 0;
-    };
+// 셰이더
+struct ShaderDesc {
+    std::string file_path;
+    // shader stage, entry point, etc.
+};
 
-    // 렌더러 인터페이스
-    class IRenderer
-    {
-    public:
-        virtual ~IRenderer() = default;
+// 파이프라인 (그래픽스 / 컴퓨트 구분 가능)
+struct PipelineDesc {
+    ShaderHandle vertex_shader = 0;
+    ShaderHandle fragment_shader = 0;
+    // 앞으로의 과제: compute_shader, geometry_shader 등
+};
 
-        // 스왑체인
-        virtual SwapChainHandle CreateSwapChain(const SwapChainDesc &desc) = 0;
+// IRenderer
+class IRenderer {
+   public:
+    virtual ~IRenderer() = default;
 
-        // 버퍼
-        virtual BufferHandle CreateBuffer(const BufferDesc &desc) = 0;
-        virtual void UpdateBuffer(BufferHandle handle, const void *data, size_t size) = 0;
-        virtual void BindBuffer(BufferHandle handle, uint32_t bind_point) = 0;
+    // 스왑체인
+    virtual SwapChainHandle CreateSwapChain(const SwapChainDesc& desc) = 0;
 
-        // 텍스처
-        virtual TextureHandle CreateTexture(const TextureDesc &desc,
-                                            const void *initial_data = nullptr) = 0;
-        virtual void BindTexture(TextureHandle handle, uint32_t bind_point) = 0;
+    // 버퍼
+    virtual BufferHandle CreateBuffer(const BufferDesc& desc) = 0;
+    virtual void UpdateBuffer(BufferHandle handle, const void* data, size_t size) = 0;
+    virtual void BindBuffer(BufferHandle handle, uint32_t bind_point) = 0;
 
-        // 샘플러
-        virtual SamplerHandle CreateSampler(const SamplerDesc &desc) = 0;
-        virtual void BindSampler(SamplerHandle handle, uint32_t bind_point) = 0;
+    // 텍스처
+    virtual TextureHandle CreateTexture(const TextureDesc& desc, const void* initial_data = nullptr) = 0;
+    virtual void BindTexture(TextureHandle handle, uint32_t bind_point) = 0;
 
-        // 셰이더
-        virtual ShaderHandle CreateShader(const ShaderDesc &desc) = 0;
-        virtual void ReleaseShader(ShaderHandle handle) = 0;
+    // 샘플러
+    virtual SamplerHandle CreateSampler(const SamplerDesc& desc) = 0;
+    virtual void BindSampler(SamplerHandle handle, uint32_t bind_point) = 0;
 
-        // 파이프라인
-        virtual PipelineHandle CreatePipeline(const PipelineDesc &desc) = 0;
-        virtual void BindPipeline(PipelineHandle handle) = 0;
+    // 셰이더
+    virtual ShaderHandle CreateShader(const ShaderDesc& desc) = 0;
+    virtual void ReleaseShader(ShaderHandle handle) = 0;
 
-        // 리소스 해제
-        virtual void ReleaseResource(uint64_t handle) = 0;
+    // 파이프라인
+    virtual PipelineHandle CreatePipeline(const PipelineDesc& desc) = 0;
+    virtual void BindPipeline(PipelineHandle handle) = 0;
 
-        // 프레임
-        virtual void BeginFrame() = 0;
-        virtual void EndFrame() = 0;
+    // 리소스 해제
+    virtual void ReleaseResource(uint64_t handle) = 0;
 
-        // 스태틱 함수 -> VulkanRenderer 생성
-        static std::unique_ptr<IRenderer> Create();
-    };
+    // 프레임
+    virtual void BeginFrame() = 0;
+    virtual void EndFrame() = 0;
 
-} // namespace Lumora
+    // 스태틱 함수
+    static std::unique_ptr<IRenderer> Create();
+};
 
-#endif // LUMORA_IRENDERER_H_
+}  // namespace Lumora
+
+#endif  // LUMORA_IRENDERER_H_
--- LumoraRenderer/src/VulkanRenderer.h
+++ LumoraRenderer/src/VulkanRenderer.h
@@ -1,127 +1,133 @@
-#ifndef LUMORA_VULKANRENDERER_H_
-#define LUMORA_VULKANRENDERER_H_
+#pragma once
 
 #include "../include/IRenderer.h"
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 #include <vulkan/vulkan.hpp>
 
-#include <vector>
+// #include <vk_mem_alloc.h> // VMA 사용할 경우
+// ...
+
 #include <unordered_map>
+#include <vector>
 
-namespace Lumora
-{
+namespace Lumora {
 
-    class VulkanRenderer : public IRenderer
-    {
-    public:
-        VulkanRenderer();
-        ~VulkanRenderer() override;
+class VulkanRenderer : public IRenderer {
+   public:
+    VulkanRenderer();
+    ~VulkanRenderer() override;
 
-        // IRenderer 구현
-        SwapChainHandle CreateSwapChain(const SwapChainDesc &desc) override;
+    // IRenderer 구현
+    // 스왑체인
+    SwapChainHandle CreateSwapChain(const SwapChainDesc& desc) override;
 
-        BufferHandle CreateBuffer(const BufferDesc &desc) override;
-        void UpdateBuffer(BufferHandle handle, const void *data, size_t size) override;
-        void BindBuffer(BufferHandle handle, uint32_t bind_point) override;
+    // 버퍼
+    BufferHandle CreateBuffer(const BufferDesc& desc) override;
+    void UpdateBuffer(BufferHandle handle, const void* data, size_t size) override;
+    void BindBuffer(BufferHandle handle, uint32_t bind_point) override;
 
-        TextureHandle CreateTexture(const TextureDesc &desc,
-                                    const void *initial_data) override;
-        void BindTexture(TextureHandle handle, uint32_t bind_point) override;
+    // 텍스처
+    TextureHandle CreateTexture(const TextureDesc& desc, const void* initial_data) override;
+    void BindTexture(TextureHandle handle, uint32_t bind_point) override;
 
-        SamplerHandle CreateSampler(const SamplerDesc &desc) override;
-        void BindSampler(SamplerHandle handle, uint32_t bind_point) override;
+    // 샘플러
+    SamplerHandle CreateSampler(const SamplerDesc& desc) override;
+    void BindSampler(SamplerHandle handle, uint32_t bind_point) override;
 
-        ShaderHandle CreateShader(const ShaderDesc &desc) override;
-        void ReleaseShader(ShaderHandle handle) override;
+    // 셰이더
+    ShaderHandle CreateShader(const ShaderDesc& desc) override;
+    void ReleaseShader(ShaderHandle handle) override;
 
-        PipelineHandle CreatePipeline(const PipelineDesc &desc) override;
-        void BindPipeline(PipelineHandle handle) override;
+    // 파이프라인
+    PipelineHandle CreatePipeline(const PipelineDesc& desc) override;
+    void BindPipeline(PipelineHandle handle) override;
 
-        void ReleaseResource(uint64_t handle) override;
+    // 리소스 해제
+    void ReleaseResource(uint64_t handle) override;
 
-        void BeginFrame() override;
-        void EndFrame() override;
+    // 프레임
+    void BeginFrame() override;
+    void EndFrame() override;
 
-    private:
-        // 내부 구조체들
-        struct VulkanSwapChain
-        {
-            vk::SwapchainKHR swapchain;
-            vk::Extent2D extent;
-            std::vector<vk::Image> images;
-            std::vector<vk::ImageView> image_views;
-            vk::RenderPass render_pass;
-            std::vector<vk::Framebuffer> framebuffers;
-        };
-
-        struct VulkanBuffer
-        {
-            vk::Buffer buffer;
-            size_t size_in_bytes;
-        };
-
-        struct VulkanTexture
-        {
-            vk::Image image;
-            vk::ImageView image_view;
-            uint32_t width, height;
-        };
-
-        struct VulkanSampler
-        {
-            vk::Sampler sampler;
-        };
-
-        struct VulkanShader
-        {
-            vk::ShaderModule shader_module;
-        };
-
-        struct VulkanPipeline
-        {
-            vk::Pipeline pipeline;
-            vk::PipelineLayout pipeline_layout;
-        };
-
-        // 컨테이너
-        std::vector<VulkanSwapChain> swapchains_;
-        std::vector<VulkanBuffer> buffers_;
-        std::vector<VulkanTexture> textures_;
-        std::vector<VulkanSampler> samplers_;
-        std::vector<VulkanPipeline> pipelines_;
-        std::unordered_map<ShaderHandle, VulkanShader> shaders_;
-
-        // 핸들 발급
-        SwapChainHandle next_swapchain_handle_ = 1;
-        BufferHandle next_buffer_handle_ = 1;
-        TextureHandle next_texture_handle_ = 1;
-        SamplerHandle next_sampler_handle_ = 1;
-        PipelineHandle next_pipeline_handle_ = 1;
-        ShaderHandle next_shader_handle_ = 1;
-
-        // Vulkan
-        vk::Instance instance_;
-        vk::PhysicalDevice physical_device_;
-        vk::Device device_;
-        vk::Queue graphics_queue_;
-        uint32_t graphics_queue_index_ = 0;
-
-        // 다른 자원들 (커맨드 풀, 동기화 등)
-        bool created_ = false;
-
-    private:
-        void InitVulkan();
-        void CleanupVulkan();
-
-        SwapChainHandle CreateSwapChainInternal(const SwapChainDesc &desc);
-
-        vk::ShaderModule CreateShaderModule(const std::vector<char> &code);
-        std::vector<char> ReadFile(const std::string &filename);
-
-        // “앞으로의 과제”:
-        // - Win32 Surface 만들기 (vkCreateWin32SurfaceKHR)
-        // - 멀티 샘플링, 컴퓨트 파이프라인, 큐브맵, 리사이즈 지원 등
+   private:
+    // 내부 구조체
+    struct VulkanSwapChain {
+        vk::SwapchainKHR swapchain;
+        std::vector<vk::Image> images;
+        std::vector<vk::ImageView> image_views;
+        vk::Extent2D extent;
+        vk::RenderPass render_pass;
+        std::vector<vk::Framebuffer> framebuffers;
+    };
+    struct VulkanBuffer {
+        vk::Buffer buffer;
+        size_t size_in_bytes = 0;
+    };
+    struct VulkanTexture {
+        vk::Image image;
+        vk::ImageView image_view;
+        uint32_t width = 0;
+        uint32_t height = 0;
+    };
+    struct VulkanSampler {
+        vk::Sampler sampler;
+    };
+    struct VulkanShader {
+        vk::ShaderModule shader_module;
+    };
+    struct VulkanPipeline {
+        vk::Pipeline pipeline;
+        vk::PipelineLayout pipeline_layout;
     };
 
-} // namespace Lumora
+    // 리소스 보관
+    std::vector<VulkanSwapChain> swapchains_;
+    std::vector<VulkanBuffer> buffers_;
+    std::vector<VulkanTexture> textures_;
+    std::vector<VulkanSampler> samplers_;
+    std::vector<VulkanPipeline> pipelines_;
+    std::unordered_map<ShaderHandle, VulkanShader> shaders_;
 
-#endif // LUMORA_VULKANRENDERER_H_
+    // 핸들 발급
+    SwapChainHandle next_swapchain_handle_ = 1;
+    BufferHandle next_buffer_handle_ = 1;
+    TextureHandle next_texture_handle_ = 1;
+    SamplerHandle next_sampler_handle_ = 1;
+    PipelineHandle next_pipeline_handle_ = 1;
+    ShaderHandle next_shader_handle_ = 1;
+
+    // Vulkan
+    vk::Instance instance_;
+    vk::PhysicalDevice physical_device_;
+    vk::Device device_;
+    vk::Queue graphics_queue_;
+    uint32_t graphics_queue_index_ = 0;
+
+    // Win32 Window + Surface (앞으로의 과제)
+    // (아직 Surface 생성 코드는 매우 단순표기)
+    bool initialized_ = false;
+
+    // 커맨드 풀 / 버퍼 / 동기화 등
+    vk::CommandPool command_pool_;
+    std::vector<vk::CommandBuffer> command_buffers_;
+
+    // ...
+    // 앞으로의 과제: 멀티스왑체인 이미지 동기화, MSAA, 큐브맵, 컴퓨트 등
+
+   private:
+    void InitVulkan();
+    void CleanupVulkan();
+
+    // swapchain
+    SwapChainHandle CreateSwapChainInternal(const SwapChainDesc& desc);
+
+    // 헬퍼
+    vk::ShaderModule CreateShaderModule(const std::vector<char>& code);
+    std::vector<char> ReadFile(const std::string& path);
+};
+
+}  // namespace Lumora
